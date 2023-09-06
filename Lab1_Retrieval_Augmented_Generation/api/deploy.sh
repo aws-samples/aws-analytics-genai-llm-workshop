dda846410864fc0ac3ee78a4b3a838fb73441c40
bhutyani <bhutyani@amazon.com>
2023-09-06 15:27:38 -0700

    Updated to remove numpy dependencies

--- Lab1_Retrieval_Augmented_Generation/api/deploy.sh
+++ Lab1_Retrieval_Augmented_Generation/api/deploy.sh
@@ -7,15 +7,11 @@
 # all dependencies used by the lambda are installed in the deps folder
 DEPS_DIR=deps
 
-# numpy fies from pypi, note this URL is specifically for Python 3.9, would need to be changed for a different Python version
-NUMPY_WHL_URL=https://files.pythonhosted.org/packages/f4/f4/45e6e3f7a23b9023554903a122c95585e9787f9403d386bafb7a95d24c9b/numpy-1.24.2-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
-NUMPY_WHL=`basename $NUMPY_WHL_URL`
-
 # useful constants 
 FN_ZIP_FILE=function.zip
 REQS_TXT_PATH=app/requirements.txt
 APP_DIR=app
-APP_NAME=llm-apps-workshop
+APP_NAME=llm-analytics-apps-workshop
 
 # derive bucket name to put the function.zip in, to be used if the caller did not provide a bucket name
 ACCOUNT_ID=`aws sts get-caller-identity --output text --query 'Account'`
@@ -41,19 +37,10 @@ echo going to pip install dependencies listed in $REQS_TXT_PATH
 pip install -r $REQS_TXT_PATH --no-cache-dir --target=$DEPS_DIR
 echo done installing dependencies
 
-# clean up the dependencies dir to remove numpy because we will be installing it manually via the .whl file
-cd $DEPS_DIR
-rm -rf numpy*
-echo going to wget numpy via $NUMPY_WHL_URL
-wget $NUMPY_WHL_URL
-echo goig to install numpy via $NUMPY_WHL
-unzip $NUMPY_WHL  
-echo installed numpy via $NUMPY_WHL_URL
-
 # remove boto3, it is is already included in the lambda python runtime
+cd $DEPS_DIR
 rm -rf boto*
-rm -f $NUMPY_WHL
-echo delete boto and $NUMPY_WHL
+echo deleted boto 
 
 # zip up the dependencies
 echo going to package dependencies in $FN_ZIP_FILE
